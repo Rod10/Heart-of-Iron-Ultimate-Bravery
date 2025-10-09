4040c172f12a9d1818736a38944d0d34083959e0
Rod10 <erwan.billy@hotmail.fr>
2025-10-09 14:26:08 +0200

    linter fixes

--- Heart of Iron Ultimate Bravery/Models/Mod.cs
+++ Heart of Iron Ultimate Bravery/Models/Mod.cs
@@ -1,53 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Numerics;
-using Heart_of_Iron_Ultimate_Bravery.Models.Interfaces;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace Heart_of_Iron_Ultimate_Bravery.Models;
-
-public class Mod
+namespace Heart_of_Iron_Ultimate_Bravery.Models
 {
-    public string Name { get; set; }
-    public BigInteger SteamId { get; set; }
-    public string Short { get; set; }
-    public string Version { get; set; }
-    public List<Country> Countries { get; set; } = new();
-
-    public Mod() {}
-
+  public class Mod
+  {
     public Mod(string? name)
     {
-        using (StreamReader file = File.OpenText("./Data/mods.json"))
-        using (JsonTextReader reader = new JsonTextReader(file))
-        {
-            JObject rawData = (JObject)JToken.ReadFrom(reader);
-            JObject? modData = rawData.GetValue(name)!.Value<JObject>();
-            Name = modData?["name"]?.ToString() ?? "Vanilla";
-            Version = modData?["version"]?.ToString() ?? "0.0.0";
-            SteamId = BigInteger.Parse(modData?["steamId"]?.ToString()!);
-            Short = name ?? "vanilla";
-        }
+      using (StreamReader file = File.OpenText("./Data/mods.json"))
+      using (JsonTextReader reader = new JsonTextReader(file))
+      {
+        JObject rawData = (JObject)JToken.ReadFrom(reader);
+        JObject? modData = rawData.GetValue(name)!.Value<JObject>();
+        Name = modData?["name"]?.ToString() ?? "Vanilla";
+        Version = modData?["version"]?.ToString() ?? "0.0.0";
+        SteamId = BigInteger.Parse(modData?["steamId"]?.ToString()!);
+        Short = name ?? "vanilla";
+      }
     }
+
+    public string? Name { get; set; }
+
+    public BigInteger SteamId { get; set; }
+
+    public string? Short { get; set; }
+
+    public string? Version { get; set; }
+
+    public List<Country> Countries { get; set; } = new();
 
     public void AddCountries()
     {
-        // string countriesPath = $"./Data/Mods/{Name}/Files/Game/countries.json";
-        string countriesPath = $"./Data/Mods/Vanilla/Files/Game/countries.json";
-        using (StreamReader file = File.OpenText(countriesPath))
-        using (JsonTextReader reader = new JsonTextReader(file))
+      // string countriesPath = $"./Data/Mods/{Name}/Files/Game/countries.json";
+      string countriesPath = $"./Data/Mods/Vanilla/Files/Game/countries.json";
+      using (StreamReader file = File.OpenText(countriesPath))
+      using (JsonTextReader reader = new JsonTextReader(file))
+      {
+        JArray rawData = (JArray)JToken.ReadFrom(reader);
+        foreach (JToken jCountry in rawData)
         {
-            JArray rawData = (JArray)JToken.ReadFrom(reader);
-            foreach (JToken? jCountry in rawData)
-            {
-                if (jCountry["name"]?.ToString() != "")
-                {
-                    Country country = new Country(jCountry);
-                    Countries.Add(country);
-                }
-            }
+          if (jCountry["name"]?.ToString() != string.Empty)
+          {
+            Country country = new Country(jCountry);
+            Countries.Add(country);
+          }
         }
+      }
     }
+  }
 }
--- Heart of Iron Ultimate Bravery/Models/Settings.cs
+++ Heart of Iron Ultimate Bravery/Models/Settings.cs
@@ -2,113 +2,112 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using Heart_of_Iron_Ultimate_Bravery.Assets.Localization;
 using Heart_of_Iron_Ultimate_Bravery.Models.Interfaces;
 using Heart_of_Iron_Ultimate_Bravery.ViewModels;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace Heart_of_Iron_Ultimate_Bravery.Models;
-
-public class Settings : ISettings
+namespace Heart_of_Iron_Ultimate_Bravery.Models
 {
-    public string Language { get; set; }
-    public string GamePath { get; set; }
-    public Mod CurrentMod { get; set; }
-
-    public Dictionary<string ,string> ModPath { get; set; }
-
+  public class Settings : ISettings
+  {
     public Settings()
     {
-        using (StreamReader file = File.OpenText("./Data/settings.json"))
-        using (JsonTextReader reader = new JsonTextReader(file))
-        {
-            JObject rawData = (JObject)JToken.ReadFrom(reader);
-            Language = rawData.GetValue("lang")?.ToString() ?? string.Empty;
-            GamePath = rawData.GetValue("gamePath")?.ToString() ?? string.Empty;
-            CurrentMod = new Mod(rawData.GetValue("mod")?.ToString());
-            ModPath = JsonConvert.DeserializeObject<Dictionary<string, string>>(rawData.GetValue("modPath").ToString());
-        }
+      using (StreamReader file = File.OpenText("./Data/settings.json"))
+      using (var reader = new JsonTextReader(file))
+      {
+        var rawData = (JObject)JToken.ReadFrom(reader);
+        Language = rawData.GetValue("lang")?.ToString() ?? string.Empty;
+        GamePath = rawData.GetValue("gamePath")?.ToString() ?? string.Empty;
+        CurrentMod = new Mod(rawData.GetValue("mod")?.ToString());
+        ModPath = JsonConvert.DeserializeObject<Dictionary<string, string>>(rawData.GetValue("modPath").ToString());
+      }
     }
-    
+
+    public string Language { get; set; }
+
+    public string GamePath { get; set; }
+
+    public Mod CurrentMod { get; set; }
+
+    public Dictionary<string, string> ModPath { get; set; }
+
     public void SetLanguage(string language)
     {
-        try
-        {
-            Language = language;
-            UpdateData();
-
-        }
-        catch (Exception e)
-        {
-            throw new (e.Message);
-        }
+      try
+      {
+        Language = language;
+        UpdateData();
+      }
+      catch (Exception e)
+      {
+        throw new Exception(e.Message);
+      }
     }
+
     public void SetGamePath(string path = "test")
     {
-        try
-        {
-            GamePath = path;
-            UpdateData();
-
-        }
-        catch (Exception e)
-        {
-            throw new (e.Message);
-        }
+      try
+      {
+        GamePath = path;
+        UpdateData();
+      }
+      catch (Exception e)
+      {
+        throw new Exception(e.Message);
+      }
     }
 
     public void SetMod(string mod)
     {
-        try
-        {
-            CurrentMod = new Mod(mod);
-            CurrentMod.AddCountries();
-            UpdateData();
-
-        }
-        catch (Exception e)
-        {
-            throw new (e.Message);
-        }
+      try
+      {
+        CurrentMod = new Mod(mod);
+        CurrentMod.AddCountries();
+        UpdateData();
+      }
+      catch (Exception e)
+      {
+        throw new Exception(e.Message);
+      }
     }
 
     public void SetModPath(string path)
     {
-        try
-        {
-            ModPath[CurrentMod.Short] = path;
-            UpdateData();
-
-        }
-        catch (Exception e)
-        {
-            throw new (e.Message);
-        }
+      try
+      {
+        ModPath[CurrentMod.Short] = path;
+        UpdateData();
+      }
+      catch (Exception e)
+      {
+        throw new Exception(e.Message);
+      }
     }
 
     private void UpdateData()
     {
-        try
+      try
+      {
+        object test = new
         {
-            object test = new
-            {
-                lang = Language,
-                gamePath = GamePath,
-                mod = CurrentMod.Short,
-                modPath = ModPath
-            };
-            using (StreamWriter file = File.CreateText(@"./Data/settings.json"))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, test);
-            }
-            Assets.Localization.Resources.Culture = new CultureInfo(Language);
-            MainWindowViewModel? currentWindow = ServiceCollectionExtensions.GetService<MainWindowViewModel>();
-            currentWindow.UpdateView();
-        }
-        catch (Exception e)
+          lang = Language, gamePath = GamePath, mod = CurrentMod.Short, modPath = ModPath,
+        };
+        using (StreamWriter file = File.CreateText(@"./Data/settings.json"))
         {
-            throw new (e.Message);
+          var serializer = new JsonSerializer();
+          serializer.Serialize(file, test);
         }
+
+        Resources.Culture = new CultureInfo(Language);
+        MainWindowViewModel? currentWindow = ServiceCollectionExtensions.GetService<MainWindowViewModel>();
+        currentWindow.UpdateView();
+      }
+      catch (Exception e)
+      {
+        throw new Exception(e.Message);
+      }
     }
+  }
 }
