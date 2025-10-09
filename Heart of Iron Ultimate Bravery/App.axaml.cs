be349ad824f0d70aff4b38c13caa46975bac42f9
Rod10 <erwan.billy@hotmail.fr>
2025-10-09 12:04:22 +0200

    linter fixes

--- Heart of Iron Ultimate Bravery/App.axaml.cs
+++ Heart of Iron Ultimate Bravery/App.axaml.cs
@@ -1,63 +1,56 @@
-using System;
 using System.Globalization;
-using System.IO;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core.Plugins;
-using System.Linq;
 using Avalonia.Markup.Xaml;
-using Heart_of_Iron_Ultimate_Bravery.Assets.Mods.Vanilla.Localization;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Heart_of_Iron_Ultimate_Bravery.Models;
-using Heart_of_Iron_Ultimate_Bravery.Models.Interfaces;
 using Heart_of_Iron_Ultimate_Bravery.Models.Utils;
 using Heart_of_Iron_Ultimate_Bravery.ViewModels;
 using Heart_of_Iron_Ultimate_Bravery.Views;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace Heart_of_Iron_Ultimate_Bravery;
-
-public partial class App : Application
+namespace Heart_of_Iron_Ultimate_Bravery
 {
+  public class App : Application
+  {
     public override void Initialize()
     {
-        AvaloniaXamlLoader.Load(this);
+      AvaloniaXamlLoader.Load(this);
     }
 
     public override void OnFrameworkInitializationCompleted()
     {
-        ServiceCollectionExtensions.AddCommonServices();
-        Settings settings = ServiceCollectionExtensions.GetService<Settings>() ?? new Settings();
-        settings.CurrentMod.AddCountries();
-        Assets.Localization.Resources.Culture = new CultureInfo(settings.Language);
-        LocalizationFactory locFactory = ServiceCollectionExtensions.GetService<LocalizationFactory>();
-        locFactory.SetCulture(settings.CurrentMod, new CultureInfo(settings.Language));
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+      ServiceCollectionExtensions.AddCommonServices();
+      Settings settings = ServiceCollectionExtensions.GetService<Settings>() ?? new Settings();
+      settings.CurrentMod.AddCountries();
+      Assets.Localization.Resources.Culture = new CultureInfo(settings.Language);
+      LocalizationFactory? locFactory = ServiceCollectionExtensions.GetService<LocalizationFactory>();
+      locFactory?.SetCulture(settings.CurrentMod, new CultureInfo(settings.Language));
+      if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+      {
+        // Avoid duplicate validations from both Avalonia and the CommunityToolkit.
+        // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
+        // DisableAvaloniaDataAnnotationValidation();
+        desktop.MainWindow = new MainWindow
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            // DisableAvaloniaDataAnnotationValidation();
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = ServiceCollectionExtensions.GetService<MainWindowViewModel>(),
-            };
-            desktop.MainWindow.Width = 1280;
-            desktop.MainWindow.Height = 720;
-        }
-        base.OnFrameworkInitializationCompleted();
+            DataContext = ServiceCollectionExtensions.GetService<MainWindowViewModel>(),
+        };
+        desktop.MainWindow.Width = 1280;
+        desktop.MainWindow.Height = 720;
+      }
+
+      base.OnFrameworkInitializationCompleted();
     }
 
-    /*private void DisableAvaloniaDataAnnotationValidation()
-    {
-        // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
-            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
+      /*private void DisableAvaloniaDataAnnotationValidation()
+      {
+          // Get an array of plugins to remove
+          var dataValidationPluginsToRemove =
+              BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
 
-        // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove)
-        {
-            BindingPlugins.DataValidators.Remove(plugin);
-        }
-    }*/
+          // remove each entry found
+          foreach (var plugin in dataValidationPluginsToRemove)
+          {
+              BindingPlugins.DataValidators.Remove(plugin);
+          }
+      }*/
+  }
 }
--- Heart of Iron Ultimate Bravery/Constant/EnumHelper.cs
+++ Heart of Iron Ultimate Bravery/Constant/EnumHelper.cs
@@ -1,126 +1,121 @@
 ﻿using System;
 using System.Linq;
-using System.Reactive;
 
 namespace Heart_of_Iron_Ultimate_Bravery.Constant
 {
-    public static class EnumHelper
+  public static class EnumHelper
+  {
+    public static T[]? GetEnumTypeArrayForMod<T>(string modName)
+      where T : Enum
     {
-        public static T[]? GetTypeArrayFromUnitType<T>(UnitType unitType, string modName) where T : Enum
-        {
-            var enumType = typeof(T);
-            return unitType switch
-            {
-                UnitType.Ship => GetEnumTypeArrayForMod<ShipType>(modName) as T[],
-                UnitType.Tank => GetEnumTypeArrayForMod<TankType>(modName) as T[],
-                UnitType.Plane => GetEnumTypeArrayForMod<PlaneType>(modName) as T[],
-                UnitType.Division => GetEnumTypeArrayForMod<DivisionType>(modName) as T[],
-                _ => Enum.GetValues(enumType).Cast<T>().ToArray()
-            };
-        }
-        
-        public static T[]? GetEnumTypeArrayForMod<T>(string modName) where T : Enum
-        {
-            var enumType = typeof(T);
-            
-            return enumType.Name switch
-            {
-                nameof(ShipType) => GetShipTypesForMod(modName) as T[],
-                nameof(TankType) => GetTankTypesForMod(modName) as T[],
-                nameof(PlaneType) => GetPlaneTypesForMod(modName) as T[],
-                nameof(DivisionType) => GetDivisionTypesForMod(modName) as T[],
-                _ => Enum.GetValues(enumType).Cast<T>().ToArray()
-            };
-        }
+      Type enumType = typeof(T);
 
-        public static T[]? GetEnumVersionArrayForMod<T>(string modName) where T : Enum
-        {
-            var enumType = typeof(T);
-            
-            return enumType.Name switch
-            {
-                // nameof(ShipType) => GetShipTypesForMod(modName) as T[],
-                nameof(TankVersion) => GetTankVersionsForMod(modName) as T[],
-                // nameof(PlaneType) => GetPlaneTypesForMod(modName) as T[],
-                // nameof(DivisionType) => GetDivisionTypesForMod(modName) as T[],
-                _ => Enum.GetValues(enumType).Cast<T>().ToArray()
-            };
-        }
-
-        private static ShipType[] GetShipTypesForMod(string modName)
-        {
-            return modName.ToLower() switch
-            {
-                "vanilla" => ModConstants.ShipTypes.Vanilla,
-                // "kaiserreich" => ModConstants.ShipTypes.Kaiserreich,
-                // "road56" => ModConstants.ShipTypes.Road56,
-                _ => ModConstants.ShipTypes.Vanilla // Default fallback
-            };
-        }
-
-        private static TankType[] GetTankTypesForMod(string modName)
-        {
-            return modName.ToLower() switch
-            {
-                "vanilla" => ModConstants.TankTypes.Vanilla,
-                // "kaiserreich" =>,
-                // "road56" =>
-                //"millenniumdawn" =>,
-                _ => ModConstants.TankTypes.Vanilla
-            };
-        }
-
-        private static TankVersion[] GetTankVersionsForMod(string modName)
-        {
-            return modName.ToLower() switch
-            {
-                "vanilla" => ModConstants.TankVersions.Vanilla,
-                // "kaiserreich" =>,
-                // "road56" =>
-                //"millenniumdawn" =>,
-                _ => ModConstants.TankVersions.Vanilla
-            };
-        }
-
-        private static TurretType[] GetTurretTypesForMod(string modName)
-        {
-            return modName.ToLower() switch
-            {
-                "vanilla" => ModConstants.TurretTypes.Vanilla,
-                // "kaiserreich" =>
-                // "rod56" =>
-                // "millenniumdawn" =>
-                _ => ModConstants.TurretTypes.Vanilla
-            };
-        }
-        
-        private static PlaneType[] GetPlaneTypesForMod(string modName)
-        {
-            return modName.ToLower() switch
-            {
-                "vanilla" => ModConstants.PlaneTypes.Vanilla,
-                // "kaiserreich" => ModConstants.PlaneTypes.Kaiserreich,
-                // "road56" => ModConstants.PlaneTypes.Road56,
-                _ => ModConstants.PlaneTypes.Vanilla
-            };
-        }
-
-        private static DivisionType[] GetDivisionTypesForMod(string modName)
-        {
-            return modName.ToLower() switch
-            {
-                "vanilla" => ModConstants.DivisionTypes.Vanilla,
-                // "kaiserreich" => ModConstants.DivisionTypes.Kaiserreich,
-                // "road56" => ModConstants.DivisionTypes.Road56,
-                _ => ModConstants.DivisionTypes.Vanilla
-            };
-        }
-
-        // Helper method to check if a specific enum value is valid for a mod
-        public static bool IsValidForMod<T>(string modName, T enumValue) where T : Enum
-        {
-            var validEnums = GetEnumTypeArrayForMod<T>(modName);
-            return validEnums.Contains(enumValue);
-        }
+      return enumType.Name switch
+      {
+        nameof(ShipType) => GetShipTypesForMod(modName) as T[],
+        nameof(TankType) => GetTankTypesForMod(modName) as T[],
+        nameof(PlaneType) => GetPlaneTypesForMod(modName) as T[],
+        nameof(DivisionType) => GetDivisionTypesForMod(modName) as T[],
+        _ => Enum.GetValues(enumType).Cast<T>().ToArray(),
+      };
     }
+
+    public static T[]? GetEnumVersionArrayForMod<T>(string modName)
+      where T : Enum
+    {
+      Type enumType = typeof(T);
+
+      return enumType.Name switch
+      {
+        // nameof(ShipType) => GetShipTypesForMod(modName) as T[],
+        nameof(TankVersion) => GetTankVersionsForMod(modName) as T[],
+
+        // nameof(PlaneType) => GetPlaneTypesForMod(modName) as T[],
+        // nameof(DivisionType) => GetDivisionTypesForMod(modName) as T[],
+        _ => Enum.GetValues(enumType).Cast<T>().ToArray(),
+      };
+    }
+
+    private static ShipType[] GetShipTypesForMod(string modName)
+    {
+      return modName.ToLower() switch
+      {
+        "vanilla" => ModConstants.ShipTypes.Vanilla,
+
+        // "kaiserreich" => ModConstants.ShipTypes.Kaiserreich,
+        // "road56" => ModConstants.ShipTypes.Road56,
+        _ => ModConstants.ShipTypes.Vanilla, // Default fallback
+      };
+    }
+
+    private static TankType[] GetTankTypesForMod(string modName)
+    {
+      return modName.ToLower() switch
+      {
+        "vanilla" => ModConstants.TankTypes.Vanilla,
+
+        // "kaiserreich" =>,
+        // "road56" =>
+        // "millenniumdawn" =>,
+        _ => ModConstants.TankTypes.Vanilla,
+      };
+    }
+
+    private static TankVersion[] GetTankVersionsForMod(string modName)
+    {
+      return modName.ToLower() switch
+      {
+        "vanilla" => ModConstants.TankVersions.Vanilla,
+
+        // "kaiserreich" =>,
+        // "road56" =>
+        // "millenniumdawn" =>,
+        _ => ModConstants.TankVersions.Vanilla,
+      };
+    }
+
+    private static TurretType[] GetTurretTypesForMod(string modName)
+    {
+      return modName.ToLower() switch
+      {
+        "vanilla" => ModConstants.TurretTypes.Vanilla,
+
+        // "kaiserreich" =>
+        // "rod56" =>
+        // "millenniumdawn" =>
+        _ => ModConstants.TurretTypes.Vanilla,
+      };
+    }
+
+    private static PlaneType[] GetPlaneTypesForMod(string modName)
+    {
+      return modName.ToLower() switch
+      {
+        "vanilla" => ModConstants.PlaneTypes.Vanilla,
+
+        // "kaiserreich" => ModConstants.PlaneTypes.Kaiserreich,
+        // "road56" => ModConstants.PlaneTypes.Road56,
+        _ => ModConstants.PlaneTypes.Vanilla,
+      };
+    }
+
+    private static DivisionType[] GetDivisionTypesForMod(string modName)
+    {
+      return modName.ToLower() switch
+      {
+        "vanilla" => ModConstants.DivisionTypes.Vanilla,
+
+        // "kaiserreich" => ModConstants.DivisionTypes.Kaiserreich,
+        // "road56" => ModConstants.DivisionTypes.Road56,
+        _ => ModConstants.DivisionTypes.Vanilla,
+      };
+    }
+
+    // Helper method to check if a specific enum value is valid for a mod
+    /* public static bool IsValidForMod<T>(string modName, T enumValue) where T : Enum
+    {
+        var validEnums = GetEnumTypeArrayForMod<T>(modName);
+        return validEnums.Contains(enumValue);
+    }*/
+  }
 }
