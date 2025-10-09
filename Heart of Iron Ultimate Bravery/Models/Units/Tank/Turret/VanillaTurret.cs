a2c3d1e8a66e417d133ec677fa5666fe40984fac
Rod10 <erwan.billy@hotmail.fr>
2025-10-09 17:10:10 +0200

    some fixes and linter warnings

--- Heart of Iron Ultimate Bravery/Models/Units/Tank/Turret/VanillaTurret.cs
+++ Heart of Iron Ultimate Bravery/Models/Units/Tank/Turret/VanillaTurret.cs
@@ -8,46 +8,81 @@ using Heart_of_Iron_Ultimate_Bravery.Constant;
 using Heart_of_Iron_Ultimate_Bravery.Models.Units.Tank.Cannon;
 using Heart_of_Iron_Ultimate_Bravery.Models.Utils;
 
-namespace Heart_of_Iron_Ultimate_Bravery.Models.Units.Tank.Turret;
-
-public class VanillaTurret : BaseTurret
+namespace Heart_of_Iron_Ultimate_Bravery.Models.Units.Tank.Turret
 {
+  public class VanillaTurret : BaseTurret
+  {
 
     public int Crew;
     public List<VanillaCannon.CannonSize> AllowedCannon;
-    
+
     public VanillaTurret(TurretType type)
     {
-        Random rnd = new Random();
-        Type = type;
-        Console.WriteLine("Turret Type: " + Type);
+      var rnd = new Random();
+      Type = type;
+      Console.WriteLine("Turret Type: " + Type);
 
-        string fileName = "Data/Mods/Vanilla/Data/Tank/Turret.json";
-        string jsonString = File.ReadAllText(fileName);
-        JsonObject jsonData = JsonSerializer.Deserialize<JsonObject>(jsonString)!;
-        JsonObject jsonTurret = JsonSerializer.Deserialize<JsonObject>(jsonData[Type.ToString().ToLower()]["crew"])!;
-        Crew = rnd.Next(0, jsonTurret.Count);
-        Console.WriteLine("Crew: " + Crew);
-        List<string> allowedCannon = JsonSerializer.Deserialize<List<string>>(jsonTurret[Crew]["allowedGun"]);
-        AllowedCannon = allowedCannon.Select(x => (VanillaCannon.CannonSize) Enum.Parse(typeof(VanillaCannon.CannonSize), x.FirstCharToUpper())).ToList();
+      string fileName = "Data/Mods/Vanilla/Data/Tank/Turret.json";
+      string jsonString = File.ReadAllText(fileName);
+      JsonObject jsonData = JsonSerializer.Deserialize<JsonObject>(jsonString)!;
+      List<int> allowedCrew = GetAllowedCrew(Type);
+      Crew = allowedCrew[rnd.Next(0, allowedCrew.Count)];
+      Console.WriteLine("Crew: " + Crew);
+      List<string> allowedCannon = jsonData[Type.ToString().FirstCharToLower()]["crew"][Crew.ToString()]["allowedGun"].Deserialize<List<string>>();
+      AllowedCannon = allowedCannon.Select(x => (VanillaCannon.CannonSize)Enum.Parse(typeof(VanillaCannon.CannonSize), x.FirstCharToUpper())).ToList();
     }
-    
+
     public override void InitializeModSpecificProperties()
     {
     }
 
-    public override string GetModIdentifier() => "vanilla";
+    public override string GetModIdentifier()
+    {
+      return "vanilla";
+    }
 
     public static List<TurretType> GetAllowedTurret(TankType type)
     {
-        return type switch
-        {
-            TankType.Light => [TurretType.Light],
-            TankType.Medium => [TurretType.Medium],
-            TankType.Heavy => [TurretType.Light, TurretType.Medium, TurretType.Large],
-            TankType.SuperHeavy => [TurretType.Light, TurretType.Medium, TurretType.Large, TurretType.SuperHeavy],
-            TankType.Modern => [TurretType.Light, TurretType.Medium, TurretType.Large],
-            _ => throw new ArgumentOutOfRangeException()
-        };
+      return type switch
+      {
+        TankType.Light => [TurretType.Light],
+        TankType.Medium => [TurretType.Medium],
+        TankType.Heavy => [TurretType.Light, TurretType.Medium, TurretType.Large],
+        TankType.SuperHeavy => [TurretType.Light, TurretType.Medium, TurretType.Large, TurretType.SuperHeavy],
+        TankType.Modern => [TurretType.Light, TurretType.Medium, TurretType.Large, TurretType.Modern],
+        _ => throw new ArgumentOutOfRangeException()
+      };
     }
+
+    private List<int> GetAllowedCrew(TurretType type)
+    {
+      return type switch
+      {
+        TurretType.Light => new List<int>
+        {
+          0, 1, 2, 3,
+        },
+        TurretType.Medium => new List<int>
+        {
+          0, 1, 2, 3,
+        },
+        TurretType.Large => new List<int>
+        {
+          0, 2, 3,
+        },
+        TurretType.SuperHeavy => new List<int>
+        {
+          3, 4,
+        },
+        TurretType.Modern => new List<int>
+        {
+          0,
+        },
+        _ => new List<int>
+        {
+          1, 2,
+        },
+      };
+    }
+  }
 }
--- Heart of Iron Ultimate Bravery/ViewModels/Panel/GenerateViewModel.cs
+++ Heart of Iron Ultimate Bravery/ViewModels/Panel/GenerateViewModel.cs
@@ -1,253 +1,270 @@
 ﻿using System;
-using Avalonia;
-using Avalonia.Controls;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Reactive;
+using Avalonia;
 using Avalonia.Layout;
+using Avalonia.Media.Imaging;
 using Heart_of_Iron_Ultimate_Bravery.Constant;
 using Heart_of_Iron_Ultimate_Bravery.Models;
-using Heart_of_Iron_Ultimate_Bravery.Models.Interfaces;
 using Heart_of_Iron_Ultimate_Bravery.Models.Utils;
 using ReactiveUI;
 
-namespace Heart_of_Iron_Ultimate_Bravery.ViewModels.Panel;
-
-public class GenerateViewModel : PanelViewModelBase
+namespace Heart_of_Iron_Ultimate_Bravery.ViewModels.Panel
 {
+  public class GenerateViewModel : PanelViewModelBase
+  {
     private bool _allCountries;
     private int _selectedCountryIndex;
     private Country _selectedCountry;
     private List<string> _cbCountriesName;
-    
-    public ObservableCollection<ButtonItem> ButtonsRow1 { get; set; } = new();
-    public ObservableCollection<ButtonItem> ButtonsRow2 { get; set; } = new();
-    
+
+    public ObservableCollection<ButtonItem> ButtonsRow1 { get; set; } = new ObservableCollection<ButtonItem>();
+    public ObservableCollection<ButtonItem> ButtonsRow2 { get; set; } = new ObservableCollection<ButtonItem>();
+
     public GenerateViewModel()
     {
-        CbCountriesName = new();
-        Settings settings = ServiceCollectionExtensions.GetService<Settings>();
-        LocalizationFactory locFactory = ServiceCollectionExtensions.GetService<LocalizationFactory>();
-        foreach (Country country in settings.CurrentMod.Countries)
-        {
-            CbCountriesName.Add(locFactory.GetString(settings.CurrentMod.Short, $"{country.Name}Text"));
-        }
-        SelectedCountryIndex = 0;
-        UpdateView();
-    }   
-    
+      CbCountriesName = new List<string>();
+      Settings settings = ServiceCollectionExtensions.GetService<Settings>();
+      LocalizationFactory locFactory = ServiceCollectionExtensions.GetService<LocalizationFactory>();
+      foreach (Country country in settings.CurrentMod.Countries)
+      {
+        CbCountriesName.Add(locFactory.GetString(settings.CurrentMod.Short, $"{country.Name}Text"));
+      }
+
+      SelectedCountryIndex = 0;
+      UpdateView();
+    }
+
     public bool AllCountries
     {
-        get { return _allCountries; }
-        set { this.RaiseAndSetIfChanged(ref _allCountries, value); }
+      get => _allCountries;
+      set => this.RaiseAndSetIfChanged(ref _allCountries, value);
     }
 
     public int SelectedCountryIndex
     {
-        get { return _selectedCountryIndex; }
-        set
-        {
-            Settings settings = ServiceCollectionExtensions.GetService<Settings>();
-            this.RaiseAndSetIfChanged(ref _selectedCountryIndex, value);
-            SelectedCountry = settings.CurrentMod.Countries[_selectedCountryIndex];
-        }
+      get => _selectedCountryIndex;
+      set
+      {
+        Settings settings = ServiceCollectionExtensions.GetService<Settings>();
+        this.RaiseAndSetIfChanged(ref _selectedCountryIndex, value);
+        SelectedCountry = settings.CurrentMod.Countries[_selectedCountryIndex];
+      }
     }
 
     public Country SelectedCountry
     {
-        get { return _selectedCountry; }
-        set { this.RaiseAndSetIfChanged(ref _selectedCountry, value); }
+      get => _selectedCountry;
+      set => this.RaiseAndSetIfChanged(ref _selectedCountry, value);
     }
 
     public List<string> CbCountriesName
     {
-        get { return _cbCountriesName; }
-        set { this.RaiseAndSetIfChanged(ref _cbCountriesName, value); }
+      get => _cbCountriesName;
+      set => this.RaiseAndSetIfChanged(ref _cbCountriesName, value);
     }
 
     public void UpdateView()
-    {    // Clear previous content
-        ButtonsRow1.Clear();
-        ButtonsRow2.Clear();
-        
-        WindowsManagement? windowsManagement = ServiceCollectionExtensions.GetService<WindowsManagement>();
-        if (windowsManagement == null) throw new SystemException("windowsManagement is null");
-    
-        // Render appropriate panel based on selected unit type
-        switch (windowsManagement.UnitType)
-        {
-            case UnitType.Ship:
-                RenderShipGeneratePanel();
-                break;
-            case UnitType.Tank:
-                RenderTankGeneratePanel();
-                break;
-            case UnitType.Plane:
-                RenderPlaneGeneratePanel();
-                break;
-            case UnitType.Division:
-                RenderDivisionGeneratePanel();
-                break;
-            case UnitType.Doctrine:
-                RenderDoctrineGeneratePanel();
-                break;
-            default:
-                RenderTankGeneratePanel(); // Default fallback
-                break;
-        }
+    { // Clear previous content
+      ButtonsRow1.Clear();
+      ButtonsRow2.Clear();
+
+      WindowsManagement? windowsManagement = ServiceCollectionExtensions.GetService<WindowsManagement>();
+      if (windowsManagement == null)
+      {
+        throw new SystemException("windowsManagement is null");
+      }
+
+      // Render appropriate panel based on selected unit type
+      switch (windowsManagement.UnitType)
+      {
+        case UnitType.Ship:
+          RenderShipGeneratePanel();
+          break;
+        case UnitType.Tank:
+          RenderTankGeneratePanel();
+          break;
+        case UnitType.Plane:
+          RenderPlaneGeneratePanel();
+          break;
+        case UnitType.Division:
+          RenderDivisionGeneratePanel();
+          break;
+        case UnitType.Doctrine:
+          RenderDoctrineGeneratePanel();
+          break;
+        default:
+          RenderTankGeneratePanel(); // Default fallback
+          break;
+      }
     }
 
     private void RenderShipGeneratePanel()
     {
-        Settings settings =  ServiceCollectionExtensions.GetService<Settings>();
-        ShipType[] validShipTypes = EnumHelper.GetEnumTypeArrayForMod<ShipType>(settings.CurrentMod.Short);
+      Settings settings = ServiceCollectionExtensions.GetService<Settings>();
+      ShipType[] validShipTypes = EnumHelper.GetEnumTypeArrayForMod<ShipType>(settings.CurrentMod.Short);
 
-        int i = 0;
+      int i = 0;
 
-        foreach (ShipType shipType in validShipTypes)
+      foreach (ShipType shipType in validShipTypes)
+      {
+        string[] modFiles = Directory.GetFiles(@$"./Assets/Mods/{settings.CurrentMod.Name}/Images/Units/Ship/Type", $"*{shipType.ToString()}*");
+        string modFile = modFiles[0];
+        Bitmap buttonImage = ImageHelper.LoadFromResource(modFile);
+        string name = $"Ship_{shipType.ToString()}";
+        if (i >= 0 && i <= 2)
         {
-            var modFiles = Directory.GetFiles(@$"./Assets/Mods/{settings.CurrentMod.Name}/Images/Units/Ship/Type", $"*{shipType.ToString()}*");
-            var modFile = modFiles[0];
-            var buttonImage = ImageHelper.LoadFromResource(modFile);
-            string name = $"Ship_{shipType.ToString()}";
-            if (i >= 0 && i <= 2)
-            {
-                Thickness thickness = new Thickness();
-                HorizontalAlignment horizontalAlignment = HorizontalAlignment.Left;
-                switch (i)
-                {
-                    case 0:
-                    {
-                        thickness = new Thickness(56, 0, 0, 0);
-                        break;
-                    }
-                    case 1:
-                    {
-                        thickness = new Thickness(0, 0, 0, 0);
-                        horizontalAlignment = HorizontalAlignment.Center;
-                        break;
-                    }
-                    case 2:
-                    {
-                        thickness = new Thickness(0, 0, 56, 0);
-                        horizontalAlignment = HorizontalAlignment.Right;
-                        break;
-                    }
-                }
-                ButtonsRow1.Add(new ButtonItem(name, thickness, buttonImage, horizontalAlignment));
-            }
-            else
-            {
-                Thickness thickness = new Thickness();
-                HorizontalAlignment horizontalAlignment = HorizontalAlignment.Left;
-                switch (i)
-                {
-                    case 3:
-                    {
-                        thickness = new Thickness(56, 0, 0, 0);
-                        break;
-                    }
-                    case 4:
-                    {
-                        thickness = new Thickness(0, 0, 0, 0);
-                        horizontalAlignment = HorizontalAlignment.Center;
-                        break;
-                    }
-                    case 5:
-                    {
-                        thickness = new Thickness(0, 0, 56, 0);
-                        horizontalAlignment = HorizontalAlignment.Right;
-                        break;
-                    }
-                }
-                ButtonsRow2.Add(new ButtonItem(name, thickness, buttonImage, horizontalAlignment));
-            }
-            i++;
+          var thickness = default(Thickness);
+          HorizontalAlignment horizontalAlignment = HorizontalAlignment.Left;
+          switch (i)
+          {
+            case 0:
+              {
+                thickness = new Thickness(56, 0, 0, 0);
+                break;
+              }
+
+            case 1:
+              {
+                thickness = new Thickness(0, 0, 0, 0);
+                horizontalAlignment = HorizontalAlignment.Center;
+                break;
+              }
+
+            case 2:
+              {
+                thickness = new Thickness(0, 0, 56, 0);
+                horizontalAlignment = HorizontalAlignment.Right;
+                break;
+              }
+          }
+
+          ButtonsRow1.Add(new ButtonItem(name, thickness, buttonImage, horizontalAlignment));
         }
+        else
+        {
+          var thickness = default(Thickness);
+          HorizontalAlignment horizontalAlignment = HorizontalAlignment.Left;
+          switch (i)
+          {
+            case 3:
+              {
+                thickness = new Thickness(56, 0, 0, 0);
+                break;
+              }
+
+            case 4:
+              {
+                thickness = new Thickness(0, 0, 0, 0);
+                horizontalAlignment = HorizontalAlignment.Center;
+                break;
+              }
+
+            case 5:
+              {
+                thickness = new Thickness(0, 0, 56, 0);
+                horizontalAlignment = HorizontalAlignment.Right;
+                break;
+              }
+          }
+
+          ButtonsRow2.Add(new ButtonItem(name, thickness, buttonImage, horizontalAlignment));
+        }
+
+        i++;
+      }
     }
 
     private void RenderTankGeneratePanel()
     {
-        Settings settings =  ServiceCollectionExtensions.GetService<Settings>();
-        TankType[] validTankTypes = EnumHelper.GetEnumTypeArrayForMod<TankType>(settings.CurrentMod.Short);
+      Settings settings = ServiceCollectionExtensions.GetService<Settings>();
+      TankType[] validTankTypes = EnumHelper.GetEnumTypeArrayForMod<TankType>(settings.CurrentMod.Short);
 
-        int i = 0;
+      int i = 0;
 
-        foreach (TankType tankType in validTankTypes)
+      foreach (TankType tankType in validTankTypes)
+      {
+        string name = $"Tank_{tankType.ToString()}";
+        string[] modFiles = Directory.GetFiles(@$"./Assets/Mods/{settings.CurrentMod.Name}/Images/Units/Tank/Type", $"*{tankType.ToString()}*");
+        string modFile = modFiles[0];
+        Bitmap buttonImage = ImageHelper.LoadFromResource(modFile);
+        if (i >= 0 && i <= 2)
         {
-            string name = $"Tank_{tankType.ToString()}";
-            var modFiles = Directory.GetFiles(@$"./Assets/Mods/{settings.CurrentMod.Name}/Images/Units/Tank/Type", $"*{tankType.ToString()}*");
-            var modFile = modFiles[0];
-            var buttonImage = ImageHelper.LoadFromResource(modFile);
-            if (i >= 0 && i <= 2)
-            {
-                Thickness thickness = new Thickness();
-                HorizontalAlignment horizontalAlignment = HorizontalAlignment.Left;
-                switch (i)
-                {
-                    case 0:
-                    {
-                        thickness = new Thickness(56, 0, 0, 0);
-                        break;
-                    }
-                    case 1:
-                    {
-                        thickness = new Thickness(0, 0, 0, 0);
-                        horizontalAlignment = HorizontalAlignment.Center;
-                        break;
-                    }
-                    case 2:
-                    {
-                        thickness = new Thickness(0, 0, 56, 0);
-                        horizontalAlignment = HorizontalAlignment.Right;
-                        break;
-                    }
-                }
-                ButtonsRow1.Add(new ButtonItem(name, thickness, buttonImage, horizontalAlignment));
-            }
-            else
-            {
-                Thickness thickness = new Thickness();
-                HorizontalAlignment horizontalAlignment = HorizontalAlignment.Left;
-                switch (i)
-                {
-                    case 3:
-                    {
-                        thickness = new Thickness(56, 0, 0, 0);
-                        break;
-                    }
-                    case 4:
-                    {
-                        thickness = new Thickness(0, 0, 0, 0);
-                        horizontalAlignment = HorizontalAlignment.Center;
-                        break;
-                    }
-                    case 5:
-                    {
-                        thickness = new Thickness(0, 0, 56, 0);
-                        horizontalAlignment = HorizontalAlignment.Right;
-                        break;
-                    }
-                }
-                ButtonsRow2.Add(new ButtonItem(name, thickness, buttonImage, horizontalAlignment));
-            }
-            i++;
+          var thickness = default(Thickness);
+          HorizontalAlignment horizontalAlignment = HorizontalAlignment.Left;
+          switch (i)
+          {
+            case 0:
+              {
+                thickness = new Thickness(56, 0, 0, 0);
+                break;
+              }
+
+            case 1:
+              {
+                thickness = new Thickness(0, 0, 0, 0);
+                horizontalAlignment = HorizontalAlignment.Center;
+                break;
+              }
+
+            case 2:
+              {
+                thickness = new Thickness(0, 0, 56, 0);
+                horizontalAlignment = HorizontalAlignment.Right;
+                break;
+              }
+          }
+
+          ButtonsRow1.Add(new ButtonItem(name, thickness, buttonImage, horizontalAlignment));
         }
+        else
+        {
+          var thickness = default(Thickness);
+          HorizontalAlignment horizontalAlignment = HorizontalAlignment.Left;
+          switch (i)
+          {
+            case 3:
+              {
+                thickness = new Thickness(56, 0, 0, 0);
+                break;
+              }
+
+            case 4:
+              {
+                thickness = new Thickness(0, 0, 0, 0);
+                horizontalAlignment = HorizontalAlignment.Center;
+                break;
+              }
+
+            case 5:
+              {
+                thickness = new Thickness(0, 0, 56, 0);
+                horizontalAlignment = HorizontalAlignment.Right;
+                break;
+              }
+          }
+
+          ButtonsRow2.Add(new ButtonItem(name, thickness, buttonImage, horizontalAlignment));
+        }
+
+        i++;
+      }
     }
 
     private void RenderPlaneGeneratePanel()
     {
-        Console.WriteLine("RenderPlaneGeneratePanel");
+      Console.WriteLine("RenderPlaneGeneratePanel");
     }
 
     private void RenderDivisionGeneratePanel()
     {
-        Console.WriteLine("RenderDivisionGeneratePanel");
+      Console.WriteLine("RenderDivisionGeneratePanel");
     }
 
     private void RenderDoctrineGeneratePanel()
     {
-        Console.WriteLine("RenderDoctrineGeneratePanel");
+      Console.WriteLine("RenderDoctrineGeneratePanel");
     }
+  }
 }
